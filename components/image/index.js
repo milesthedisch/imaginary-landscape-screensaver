d307786df4c8c218745c3b2a2586dbaf8cf84a95
Miles Disch <milesthedisch@gmail.com>
2018-11-15 15:01:03 +1100

    Update image component with more data from api.

--- components/image/index.js
+++ components/image/index.js
@@ -1,11 +1,22 @@
 import { h, Component } from 'preact';
 import style from './style.scss';
-
+import Lightbox from '../lightbox';
 
 export default class Image extends Component {
-	state = {
-		clicked: false
-	};
+    constructor(props) {
+        super(props);
+
+        const images = props.preview.images[0];
+		const thumbnail = this.chooseResolutionForThumbnail(images.resolutions);
+
+        this.state = {
+          thumbnail,
+          permalink: props.permalink,
+          allImages: images.resolutions,
+          sourceImage: images.source.url,
+          clicked: false,
+        };
+    }
 
 	handleClick() {
 		this.setState({ clicked: true });
@@ -28,11 +39,18 @@ export default class Image extends Component {
 		return resolutions[0];
 	}
 
-	render({ ...props }, { clicked } = {}) {
-		const thumbnail = this.chooseResolutionForThumbnail(props.resolutions);
+	render({ ...props }, { clicked, thumbnail } = {}) {
+        if (clicked) {
+            return (
+                <Lightbox>
+                    <div class={style.image} style={this.backgroundImage(thumbnail.url)} onClick={() => this.handleClick()} />
+                </Lightbox>
+            );
+        }
 
 		return (
-			<div class={style.image} style={this.backgroundImage(thumbnail.url)} onClick={this.handleClick} />
+			<div class={style.image} style={this.backgroundImage(thumbnail.url)} onClick={() => this.handleClick()} />
 		);
 	}
 }
+
--- components/lightbox/index.js
+++ components/lightbox/index.js
@@ -1,18 +1,10 @@
 import { h, Component } from 'preact';
 import style from './style.scss';
 
-export default class LightBox extends Component {
-	// constructor() {
-	// 	super();
-	// }
-
-	state = {
-
-	};
-
-	render(props, state) {
-		return (
-			<div class={style.lightbox} />
-		);
-	}
+const Lightbox = ({ children, props }) => {
+    return (
+        <div class={style.lightbox}>{ children }</div>
+    );
 }
+
+export default Lightbox;
